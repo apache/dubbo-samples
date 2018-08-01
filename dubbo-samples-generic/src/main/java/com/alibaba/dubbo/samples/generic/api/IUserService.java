18849212ac7f1874dc7370c0c1d95c327eb81ca3
ningyu1 <ningbe111@163.com>
2018-08-01 11:11:15 +0800

    modify generic sample code

--- dubbo-samples-generic/src/main/java/com/alibaba/dubbo/samples/generic/api/IUserService.java
+++ dubbo-samples-generic/src/main/java/com/alibaba/dubbo/samples/generic/api/IUserService.java
@@ -19,50 +19,6 @@
 
 package com.alibaba.dubbo.samples.generic.api;
 
-import java.io.Serializable;
-
-import com.alibaba.dubbo.samples.generic.api.IUserService.Params;
-import com.alibaba.dubbo.samples.generic.api.IUserService.User;
-
-public interface IUserService extends IService<Params, User> {
-
-
-    public static class Params implements Serializable {
-        private static final long serialVersionUID = 1L;
-
-        public Params(String query) {
-        }
-    }
-
-    public static class User implements Serializable {
-        private static final long serialVersionUID = 1L;
-        private int id;
-        private String name;
-        public User(int id, String name) {
-            super();
-            this.id = id;
-            this.name = name;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return "User [id=" + id + ", name=" + name + "]";
-        }
-    }
+public interface IUserService {
+    User get(Params params);
 }
