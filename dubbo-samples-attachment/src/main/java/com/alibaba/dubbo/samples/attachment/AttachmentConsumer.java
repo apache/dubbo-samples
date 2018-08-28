8e1243ff0535ffa13f55ea274ef75431f2a8a12a
zijinoier <evenlove2010@gmail.com>
2018-08-28 12:20:39 +0800

    issue 1

--- dubbo-samples-attachment/src/main/java/com/alibaba/dubbo/samples/attachment/AttachmentConsumer.java
+++ dubbo-samples-attachment/src/main/java/com/alibaba/dubbo/samples/attachment/AttachmentConsumer.java
@@ -28,17 +28,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AttachmentConsumer {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring/attachment-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/attachment-consumer.xml"});
         context.start();
         AttachmentService attachmentService = (AttachmentService) context.getBean("demoService"); // get remote service proxy
 
-        RpcContext.getContext().setAttachment("index", "1");
-        String hello = attachmentService.sayHello("world");
-        System.out.println(hello); // get result
-
-
-        hello = attachmentService.sayHello("world"); //attachment only affective once
-        System.out.println(hello); // get result
-
+        while(true){
+            try{
+                Thread.sleep(1000);
+                String consumerName = context.getBeanDefinitionNames()[0];
+                RpcContext.getContext().setAttachment("name",  consumerName);
+                String hello = attachmentService.sayHello("world");
+                System.out.println(hello);
+            }catch (Throwable throwable){
+                throwable.printStackTrace();
+            }
+        }
     }
 }
