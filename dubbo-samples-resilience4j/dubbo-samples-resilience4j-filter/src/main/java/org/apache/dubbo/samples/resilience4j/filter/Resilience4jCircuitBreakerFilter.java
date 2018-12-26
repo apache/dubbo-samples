4cc70290677c1789fdf6222f2a0e967cc8197f2f
cvictory <shenglicao2@gmail.com>
2018-12-26 14:18:04 +0800

    init resilience4j project

---
+++ dubbo-samples-resilience4j/dubbo-samples-resilience4j-filter/src/main/java/org/apache/dubbo/samples/resilience4j/filter/Resilience4jCircuitBreakerFilter.java
@@ -0,0 +1,42 @@
+package org.apache.dubbo.samples.resilience4j.filter;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+
+/**
+ * @author cvictory ON 2018/12/25
+ */
+public class Resilience4jCircuitBreakerFilter implements Filter {
+
+    static CircuitBreaker circuitBreaker;
+
+    static {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(20)
+                .ringBufferSizeInClosedState(5)
+                .build();
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(config);
+        circuitBreaker = registry.circuitBreaker("my");
+    }
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        long start = System.nanoTime();
+        try {
+            Result result = invoker.invoke(invocation);
+            long durationInNanos = System.nanoTime() - start;
+            circuitBreaker.onSuccess(durationInNanos);
+            return result;
+        } catch (Throwable throwable) {
+            long durationInNanos = System.nanoTime() - start;
+            circuitBreaker.onError(durationInNanos, throwable);
+            throw throwable;
+        }
+    }
+}
