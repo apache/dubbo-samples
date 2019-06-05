d15fbbeba28d339e73d8b73faf8d68d4b6ee128d
Ian Luo <ian.luo@gmail.com>
2019-06-05 19:49:46 +0800

    simplify samples, and add integration test for dubbo-samples-attachment

--- dubbo-samples-attachment/src/main/java/org/apache/dubbo/samples/attachment/AttachmentProvider.java
+++ dubbo-samples-attachment/src/main/java/org/apache/dubbo/samples/attachment/AttachmentProvider.java
@@ -24,12 +24,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AttachmentProvider {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         new EmbeddedZooKeeper(2181, false).start();
-        System.setProperty("java.net.preferIPv4Stack", "true");
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/attachment-provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/attachment-provider.xml");
         context.start();
 
-        System.in.read(); // press any key to exit
+        System.out.println("dubbo service started.");
+        System.in.read();
     }
 }
