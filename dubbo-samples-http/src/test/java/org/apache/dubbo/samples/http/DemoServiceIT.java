5eb786b1f1462787bc5d60e141b912c88c1e63ab
Ian Luo <ian.luo@gmail.com>
2019-06-26 15:11:34 +0800

    integration test for dubbo-samples-http

---
+++ dubbo-samples-http/src/test/java/org/apache/dubbo/samples/http/DemoServiceIT.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dubbo.samples.http;
+
+import org.apache.dubbo.samples.http.api.DemoService;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/http-consumer.xml"})
+public class DemoServiceIT {
+    @Autowired
+    private DemoService demoService;
+
+    @Test
+    public void test1() throws Exception {
+        Assert.assertTrue(demoService.sayHello("world").startsWith("Hello world"));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        boolean isFromTomcat = false;
+        boolean isFromJetty = false;
+        for (int i = 0; i < 10; i++) {
+            String result = demoService.sayHello("world");
+            if (result.contains("8080")) {
+                isFromTomcat = true;
+            }
+            if (result.contains("8081")) {
+                isFromJetty = true;
+            }
+        }
+        Assert.assertTrue(isFromJetty && isFromTomcat);
+    }
+}
