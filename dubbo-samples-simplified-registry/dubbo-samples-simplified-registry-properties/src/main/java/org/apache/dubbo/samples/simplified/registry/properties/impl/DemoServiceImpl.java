eb377b675fa4a480a65a7ab758128c777b702654
Ian Luo <ian.luo@gmail.com>
2019-07-01 15:38:31 +0800

    integration test for dubbo-samples-simplified-registry-properties

--- dubbo-samples-simplified-registry/dubbo-samples-simplified-registry-properties/src/main/java/org/apache/dubbo/samples/simplified/registry/properties/impl/DemoServiceImpl.java
+++ dubbo-samples-simplified-registry/dubbo-samples-simplified-registry-properties/src/main/java/org/apache/dubbo/samples/simplified/registry/properties/impl/DemoServiceImpl.java
@@ -17,10 +17,21 @@
  *
  */
 
-package org.apache.dubbo.samples.simplified.registry.nosimple.api;
+package org.apache.dubbo.samples.simplified.registry.properties.impl;
 
-public interface DemoService {
 
-    String sayHello(String name);
+import org.apache.dubbo.rpc.RpcContext;
+import org.apache.dubbo.samples.simplified.registry.properties.api.DemoService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name +
+                ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+        return "Hello " + name + ", response from provider: " + RpcContext.getContext().getLocalAddress();
+    }
 }
