48328856efa2aaa0893fea64541766e343e45736
ken.lj <ken.lj.hz@gmail.com>
2019-07-01 20:52:28 +0800

    add nacos samples

---
+++ dubbo-samples-nacos/dubbo-samples-nacos-tagrouter/src/main/java/org/apache/dubbo/samples/governance/BasicConsumer.java
@@ -0,0 +1,60 @@
+/*
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package org.apache.dubbo.samples.governance;
+
+import org.apache.dubbo.rpc.RpcContext;
+import org.apache.dubbo.rpc.cluster.Constants;
+import org.apache.dubbo.samples.governance.api.DemoService;
+import org.apache.dubbo.samples.governance.api.DemoService2;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.apache.dubbo.rpc.Constants.FORCE_USE_TAG;
+
+public class BasicConsumer {
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-demo-consumer.xml");
+        context.start();
+        DemoService demoService = context.getBean("demoService", DemoService.class);
+        DemoService2 demoService2 = context.getBean("demoService2", DemoService2.class);
+
+        while (true) {
+            try {
+                RpcContext.getContext().setAttachment(Constants.TAG_KEY, "tag1");
+                String hello = demoService.sayHello("world");
+                System.out.println(hello);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                RpcContext.getContext().setAttachment(FORCE_USE_TAG, "true");
+                RpcContext.getContext().setAttachment(Constants.TAG_KEY, "tag2");
+                String hello2 = demoService2.sayHello("world again");
+                System.out.println(hello2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Thread.sleep(1000);
+        }
+    }
+}
