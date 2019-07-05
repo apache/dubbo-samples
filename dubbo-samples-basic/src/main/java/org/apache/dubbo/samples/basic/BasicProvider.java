b42a624921e57be2d9a2fdd4c37b50b52ed7a79c
Ian Luo <ian.luo@gmail.com>
2019-07-05 11:21:01 +0800

    clean up for dubbo-samples-basic

--- dubbo-samples-basic/src/main/java/org/apache/dubbo/samples/basic/BasicProvider.java
+++ dubbo-samples-basic/src/main/java/org/apache/dubbo/samples/basic/BasicProvider.java
@@ -21,16 +21,19 @@ package org.apache.dubbo.samples.basic;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.CountDownLatch;
+
 public class BasicProvider {
 
     public static void main(String[] args) throws Exception {
         new EmbeddedZooKeeper(2181, false).start();
         // wait for embedded zookeeper start completely.
         Thread.sleep(1000);
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-demo-provider.xml");
         context.start();
-        System.out.println("dubbo service started");
-        System.in.read();
-    }
 
+        System.out.println("dubbo service started");
+        new CountDownLatch(1).await();
+    }
 }
