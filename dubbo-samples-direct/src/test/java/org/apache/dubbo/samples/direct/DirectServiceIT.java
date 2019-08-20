9ba8ebdb333af84c1f96c9c8a4f4a1deabb6a016
Ian Luo <ian.luo@gmail.com>
2019-08-20 13:03:58 +0800

    integration test for https://github.com/apache/dubbo/issues/4889

--- dubbo-samples-direct/src/test/java/org/apache/dubbo/samples/direct/DirectServiceIT.java
+++ dubbo-samples-direct/src/test/java/org/apache/dubbo/samples/direct/DirectServiceIT.java
@@ -17,6 +17,10 @@
 
 package org.apache.dubbo.samples.direct;
 
+import junit.framework.TestCase;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.apache.dubbo.samples.direct.api.DirectService;
 
 import org.junit.Assert;
@@ -29,11 +33,45 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/spring/dubbo-direct-consumer.xml"})
 public class DirectServiceIT {
+    private static String providerAddress = System.getProperty("target.address", "localhost");
+
     @Autowired
     private DirectService directService;
 
     @Test
-    public void test() throws Exception {
+    public void testXml() throws Exception {
         Assert.assertTrue(directService.sayHello("dubbo").startsWith("Hello dubbo"));
+    }
+
+    @Test
+    public void testGeneric() throws Exception {
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("direct-consumer");
+        ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
+        reference.setUrl("dubbo://" + providerAddress + ":20880/" + DirectService.class.getName());
+        reference.setVersion("1.0.0-daily");
+        reference.setGroup("test");
+        reference.setGeneric(true);
+        reference.setApplication(application);
+        reference.setInterface(DirectService.class.getName());
+        GenericService genericService = reference.get();
+        Object obj = genericService.$invoke("sayHello", new String[]{String.class.getName()}, new Object[]{ "generic" });
+        String str = (String) obj;
+        TestCase.assertTrue(str.startsWith("Hello generic"));
+    }
+
+    @Test
+    public void testApi() throws Exception {
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("direct-consumer");
+        ReferenceConfig<DirectService> reference = new ReferenceConfig<>();
+        reference.setUrl("dubbo://" + providerAddress + ":20880/" + DirectService.class.getName());
+        reference.setVersion("1.0.0-daily");
+        reference.setGroup("test");
+        reference.setApplication(application);
+        reference.setInterface(DirectService.class.getName());
+        DirectService service = reference.get();
+        String result = service.sayHello("api");
+        TestCase.assertTrue(result.startsWith("Hello api"));
     }
 }
