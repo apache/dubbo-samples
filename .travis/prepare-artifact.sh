d52c48e30a796fe3b5b8166bd22ab68cb04a3fe2
Huang Yunkun <htynkn@gmail.com>
2019-12-26 19:49:50 +0800

    add relaese branch check

--- .travis/prepare-artifact.sh
+++ .travis/prepare-artifact.sh
@@ -1,2 +1,12 @@
 #! /usr/bin/env bash
 env
+
+if [[ -z "${RELEASE_BRANCH}" ]]; then
+  export INTERGARTION_TEST_VERSION = '2.7.6-SNAPSHOT'
+else
+  git clone --depth 1 git@github.com:apache/dubbo.git
+  cd dubbo
+  git checkout ${RELEASE_BRANCH}
+  ./mwnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
+  export INTERGARTION_TEST_VERSION = ${RELEASE_BRANCH}
+fi
