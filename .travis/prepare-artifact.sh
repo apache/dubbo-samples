6a7ca294af8a0c98c42bc89637fa30d6f5e7f6ee
Huang Yunkun <htynkn@gmail.com>
2019-12-27 10:12:53 +0800

    fix

--- .travis/prepare-artifact.sh
+++ .travis/prepare-artifact.sh
@@ -2,11 +2,11 @@
 env
 
 if [[ -z "${RELEASE_BRANCH}" ]]; then
-  export INTERGARTION_TEST_VERSION = '2.7.6-SNAPSHOT'
+  export INTERGARTION_TEST_VERSION='2.7.6-SNAPSHOT'
 else
   git clone https://github.com/apache/dubbo.git
   cd dubbo
   git checkout ${RELEASE_BRANCH}
   ./mwnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
-  export INTERGARTION_TEST_VERSION = ${RELEASE_BRANCH}
+  export INTERGARTION_TEST_VERSION=${RELEASE_BRANCH}
 fi
