5f923760626d4aac3fd5cf2599154cd47a93da61
Huang YunKun <htynkn@gmail.com>
2019-12-29 19:49:10 +0800

    add support for release verify in ci (#137)

---
+++ .travis/prepare-artifact.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+env
+
+if [[ -z "${RELEASE_BRANCH}" ]]; then
+  export INTERGARTION_TEST_VERSION=2.7.6-SNAPSHOT
+else
+  git clone https://github.com/apache/dubbo.git
+  cd dubbo
+  git checkout ${RELEASE_BRANCH}
+  ls
+  ./mvnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
+  export INTERGARTION_TEST_VERSION=${RELEASE_BRANCH}
+  cd ..
+fi
+
+env|grep INTERGARTION_TEST_VERSION
