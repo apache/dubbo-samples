f0a357196c1755055f59044aba9c24991034ddcb
Joe Zou <yixian.zou@gmail.com>
2020-01-10 18:06:58 +0800

    add router samples

--- golang/general/router/go-client/app/client.go
+++ golang/general/router/go-client/app/client.go
@@ -28,14 +28,13 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
-	"github.com/apache/dubbo-go/common"
 	"github.com/apache/dubbo-go/common/logger"
 	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
 	"github.com/apache/dubbo-go/config"
 	_ "github.com/apache/dubbo-go/protocol/dubbo"
 	_ "github.com/apache/dubbo-go/registry/protocol"
 
-	_ "github.com/apache/dubbo-go/filter/impl"
+	_ "github.com/apache/dubbo-go/filter/filter_impl"
 
 	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
 	_ "github.com/apache/dubbo-go/cluster/loadbalance"
@@ -116,8 +115,4 @@ func (u *UserProvider) Reference() string {
 
 func (User) JavaClassName() string {
 	return "com.ikurento.user.User"
-}
-
-func (u *UserProvider) CallBack(res common.CallbackResponse) {
-	fmt.Println("CallBack res:", res)
 }
--- golang/general/router/go-client/go.mod
+++ golang/general/router/go-client/go.mod
@@ -1,6 +1,12 @@
 module github.com/dubbogo/dubbo-samples/golang
 
 require (
-	github.com/apache/dubbo-go v0.1.2-0.20191219122427-56cb00a83ba8
-	github.com/apache/dubbo-go-hessian2 v1.2.5-0.20191029001541-894e45c9aaaa
+	github.com/apache/dubbo-go v1.2.0
+	github.com/apache/dubbo-go-hessian2 v1.3.1-0.20200106042936-0e1fc43955b2
 )
+
+replace github.com/apache/dubbo-go v1.2.0 => C:\workspace\github\dubbo-go\src\github.com\apache\dubbo-go
+
+//replace github.com/dubbogo/gost v1.5.1 => C:\workspace\github\gost\src\github.com\dubbogo\gost
+
+go 1.13
