2f8e81bd75636c992399ee827780ab53c22c3a76
ken.lj <ken.lj.hz@gmail.com>
2020-03-12 19:16:45 +0800

    fix assert error

--- java/dubbo-samples-metadata-report/dubbo-samples-metadata-report-configcenter/src/test/java/org/apache/dubbo/samples/metadatareport/configcenter/MetadataIT.java
+++ java/dubbo-samples-metadata-report/dubbo-samples-metadata-report-configcenter/src/test/java/org/apache/dubbo/samples/metadatareport/configcenter/MetadataIT.java
@@ -105,7 +105,7 @@ public class MetadataIT {
         Map<String, String> parameters = definition.getParameters();
         Assert.assertEquals("provider", parameters.get("side"));
         Assert.assertEquals("sayHello", parameters.get("methods"));
-        Assert.assertEquals("5000", parameters.get("timeout"));
+        Assert.assertEquals("1000", parameters.get("timeout"));
         Assert.assertEquals("1.1.1", parameters.get("version"));
         Assert.assertEquals("d-test", parameters.get("group"));
         Assert.assertEquals("100", parameters.get("threads"));
