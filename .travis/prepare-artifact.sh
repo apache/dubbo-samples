6346b5868d6a0b334485b2bf012d88a32130c55f
Huang Yunkun <htynkn@gmail.com>
2020-11-02 10:23:08 +0800

    fix version

--- .travis/prepare-artifact.sh
+++ .travis/prepare-artifact.sh
@@ -2,7 +2,7 @@
 env
 
 if [[ -z "${RELEASE_BRANCH}" ]]; then
-  export INTERGARTION_TEST_VERSION=2.7.8-SNAPSHOT
+  export INTERGARTION_TEST_VERSION=2.7.9-SNAPSHOT
 else
   git clone https://github.com/apache/dubbo.git
   cd dubbo
