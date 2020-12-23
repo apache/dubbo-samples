932b55cf76aa6ea3eece909d4a59c9f5e5d720bd
gongdewei <kylixs@qq.com>
2020-12-23 23:50:56 +0800

    clean damaged image

--- test/clean-damaged-image.sh
+++ test/clean-damaged-image.sh
@@ -8,10 +8,9 @@ docker ps -a | grep dubbo | awk '{ print $1}' | xargs -I {} docker kill {}
 echo "Removing dubbo containers .."
 docker ps -a | grep dubbo | awk '{ print $1}' | xargs -I {} docker rm {}
 
-echo "Removing dubbo images .."
-docker images | grep dubbo | awk '{ print $3}' | xargs -I {} docker rmi {}
+#echo "Removing dubbo images .."
+#docker images | grep dubbo | awk '{ print $3}' | xargs -I {} docker rmi {}
 
 echo "Removing damaged images .."
 docker image prune -f
 
-echo "Clean dubbo test images done."
