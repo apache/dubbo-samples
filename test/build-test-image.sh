5eace98e57475d2ed4318b50f289b92d4de8ec31
gongdewei <kylixs@qq.com>
2020-12-24 00:14:21 +0800

    Improve docker layer cache key

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Note: modify follow comment when need refresh github actions docker layer caching
+# Docker layer caching updated: 2020.12.23
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/dubbo-test-runner/build.sh
