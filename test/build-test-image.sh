570dcc1897b221d58177c9db71440ee0ff77c2a3
gongdewei <kylixs@qq.com>
2020-12-27 15:05:04 +0800

    support debian mirror

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Note: modify follow comment when need refresh github actions docker layer caching
-# dubbo/sample-test updated: 2020.12.25
+# dubbo/sample-test updated: 2020.12.27
+
+# Usages:
+# DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
--- test/dubbo-test-runner/build.sh
+++ test/dubbo-test-runner/build.sh
@@ -4,7 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DOCKER_DIR=$DIR/target/docker
 
-mvn clean package
+echo "Building dubbo-test-runner .."
+cd $DIR
+mvn clean package -DskipTests
 result=$?
 if [ $result -ne 0 ]; then
   echo "Build dubbo-test-runner failure"
@@ -16,4 +18,4 @@ cp -r $DIR/src/docker/* $DOCKER_DIR/
 cp $DIR/target/dubbo-test-runner-*-jar-with-dependencies.jar $DOCKER_DIR/
 
 cd $DOCKER_DIR
-docker build -t dubbo/sample-test .
+docker build -t dubbo/sample-test . --build-arg DEBIAN_MIRROR=$DEBIAN_MIRROR
