a61788939966adca14c4e025b78e6b7ba17114bb
gongdewei <kylixs@qq.com>
2020-12-30 21:15:54 +0800

    parallel build samples and test image in a step

---
+++ test/build-samples-and-test-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+test_image_log=build-test-image.log
+echo "Build test image in background .."
+bash ./build-test-image.sh &> $test_image_log &
+
+echo "Build samples .."
+BUILD_OPTS="-U --batch-mode --no-transfer-progress --settings .mvn/settings.xml"
+BUILD_OPTS="$BUILD_OPTS clean package dependency:copy-dependencies -DskipTests"
+./mvnw $BUILD_OPTS
+result=$?
+if [ $result -ne 0 ];then
+  echo "Build dubbo-samples with maven failed"
+  exit $result
+fi
+
+echo "-------------------------------------------------------------------"
+echo "Check test image result .."
+cat $test_image_log
+grep "Successfully tagged dubbo/sample-test" $test_image_log > /dev/null
