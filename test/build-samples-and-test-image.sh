18489464180dfa458ff82c75faed0d5960ca10cf
gongdewei <kylixs@qq.com>
2020-12-31 12:04:00 +0800

    build both samples and test image async

--- test/build-samples-and-test-image.sh
+++ test/build-samples-and-test-image.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-test_image_log=build-test-image.log
+test_image_log=$DIR/test-image.log
+samples_log=$DIR/samples.log
+
 export DIR=$DIR
 export test_image_log=$test_image_log
+export samples_log=$samples_log
 
 cd $DIR
-echo "Build test image in background .."
+echo "Build test image .."
 bash ./build-test-image.sh &> $test_image_log &
 build_image_pid=$!
 
@@ -14,15 +17,26 @@ echo "Build samples .."
 BUILD_OPTS="-U --batch-mode --no-transfer-progress --settings .mvn/settings.xml"
 BUILD_OPTS="$BUILD_OPTS clean package dependency:copy-dependencies -DskipTests"
 cd $DIR/..
-./mvnw $BUILD_OPTS
+./mvnw $BUILD_OPTS &> $samples_log &
+build_samples_pid=$!
+
+echo "-------------------------------------------------------------------"
+echo "Waiting test image building: $build_image_pid .."
+tail -n 1000 -f $test_image_log &
+wait $build_image_pid
+result=$?
+if [ $result -ne 0 ];then
+  echo "Build test image failed"
+  exit $result
+fi
+
+echo ""
+echo "-------------------------------------------------------------------"
+echo "Waiting samples building: $build_samples_pid .."
+tail -n 500 -f $samples_log &
+wait $build_samples_pid
 result=$?
 if [ $result -ne 0 ];then
   echo "Build dubbo-samples with maven failed"
   exit $result
 fi
-
-echo "-------------------------------------------------------------------"
-echo "Waiting test image building: $build_image_pid .."
-tail -n 1000 -f $DIR/$test_image_log &
-wait $build_image_pid
-grep "Successfully tagged dubbo/sample-test" $DIR/$test_image_log > /dev/null
