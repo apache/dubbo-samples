6f894b458adf205f80680c824649f35bfa8f84f2
superalong <superalong2014@gmail.com>
2021-01-08 12:32:36 +0800

    dubbo-samples-original-grpc's testcase convert to docker's framework (#228)

--- dubbo-samples-grpc/dubbo-samples-original/src/test/java/org/apache/dubbo/samples/basic/OriginalGrpcIT.java
+++ dubbo-samples-grpc/dubbo-samples-original/src/test/java/org/apache/dubbo/samples/basic/OriginalGrpcIT.java
@@ -24,41 +24,16 @@ import io.grpc.examples.helloworld.DubboGreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:spring/dubbo-demo-consumer.xml", "classpath*:spring/dubbo-demo-provider.xml"})
+@ContextConfiguration(locations = {"classpath*:spring/dubbo-demo-consumer.xml"})
 public class OriginalGrpcIT {
-    //
-//    static {
-//        try {
-//            GenericContainer zookeeper = new GenericContainer<>("zookeeper:3.4.9")
-//                    .withExposedPorts(2181).waitFor();
-//
-//            System.setProperty("zookeeper.address", zookeeper.getContainerIpAddress());
-//            System.setProperty("zookeeper.port", zookeeper.getFirstMappedPort() + "");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-    // port mapping, https://github.com/testcontainers/testcontainers-java/issues/256
-    @ClassRule
-    public static GenericContainer zookeeper = new GenericContainer<>(DockerImageName.parse("zookeeper:3.4.9"))
-            .withCreateContainerCmdModifier(
-                    cmd ->{
-                        cmd.withPortBindings(new PortBinding(Ports.Binding.bindPort(2181), new ExposedPort(2181)));
-                    }
-            );
 
     @Autowired
     @Qualifier("greeter")
