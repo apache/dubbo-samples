083dcc5f5e5c3c490ceab39499d1584031c1c19d
gongdewei <kylixs@qq.com>
2021-01-12 22:25:39 +0800

    force delete target dir

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -184,14 +184,12 @@ function process_case() {
     echo "$log_prefix Building project : $scenario_name with versions: $version_profile .."
     cd $project_home
 
-    # clean target
+    # clean target manual, avoid 'mvn clean' failed with 'Permission denied' in github actions
     rm -rf target
     result=$?
-    while [ $result -ne 0 ]; do
-      sleep 0.5
-      rm -rf target
-      result=$?
-    done
+    if [ $result -ne 0 ]; then
+      sudo rm -rf target
+    fi
 
     mvn $MVN_OPTS $version_profile &> $project_home/mvn.log
     result=$?
@@ -199,8 +197,6 @@ function process_case() {
       echo "$log_prefix $TEST_FAILURE: Build failure, please check log: $project_home/mvn.log" | tee -a $testResultFile
       if [ "$SHOW_ERROR_DETAIL" == "1" ];then
         cat $project_home/mvn.log
-#        echo "$log_prefix process list:"
-#        ps -ef
       fi
       return 1
     fi
