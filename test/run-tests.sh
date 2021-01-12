0390f4125163fd19ed7851e55b655d76d1735778
gongdewei <kylixs@qq.com>
2021-01-12 19:28:39 +0800

    print build log if failed

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -181,15 +181,17 @@ function process_case() {
     start_time=$SECONDS
     profile_no=$((profile_no + 1))
     log_prefix="[${case_no}/${caseCount}] [$scenario_name:$profile_no/$profile_count]"
-    echo "$log_prefix Use version profile: $version_profile"
 
     # run test using version profile
-    echo "$log_prefix Building project : $scenario_name .."
+    echo "$log_prefix Building project : $scenario_name with versions: $version_profile .."
     cd $project_home
     mvn $MVN_OPTS $version_profile &> $project_home/mvn.log
     result=$?
     if [ $result -ne 0 ]; then
       echo "$log_prefix $TEST_FAILURE: Build failure, please check log: $project_home/mvn.log" | tee -a $testResultFile
+      if [ "$SHOW_ERROR_DETAIL" == "1" ];then
+        cat $project_home/mvn.log
+      fi
       return 1
     fi
 
