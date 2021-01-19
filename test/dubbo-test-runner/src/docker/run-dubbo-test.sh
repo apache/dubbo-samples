d389a5ce7adf7ae20653db4efa15eaf4a1b4d26d
gongdewei <kylixs@qq.com>
2021-01-19 14:57:06 +0800

    fix classloader problem in java 9+ (CompletableFuture.supplyAsync())

--- test/dubbo-test-runner/src/docker/run-dubbo-test.sh
+++ test/dubbo-test-runner/src/docker/run-dubbo-test.sh
@@ -37,7 +37,11 @@ fi
 # run testcase
 echo "Running tests ..."
 report_dir=$DIR/app/test-reports
-java $JAVA_OPTS $DEBUG_OPTS -jar dubbo-test-runner.jar "$TEST_CLASSES_DIR" "$APP_CLASSES_DIR" "$APP_DEPENDENCY_DIR" "$report_dir" "$TEST_PATTERNS" 2>&1
+
+# Fix Class loading problem in java9+: CompletableFuture.supplyAsync() is executed in the ForkJoinWorkerThread
+# and it only uses system classloader to load classes instead of the IsolatedClassLoader
+classpath=dubbo-test-runner.jar:$TEST_CLASSES_DIR:$APP_CLASSES_DIR:$APP_DEPENDENCY_DIR/*
+java $JAVA_OPTS $DEBUG_OPTS -cp $classpath org.apache.dubbo.test.runner.TestRunnerMain "$TEST_CLASSES_DIR" "$APP_CLASSES_DIR" "$APP_DEPENDENCY_DIR" "$report_dir" "$TEST_PATTERNS" 2>&1
 result=$?
 if [ $result -ne 0 ]; then
   echo "Run tests failure"
