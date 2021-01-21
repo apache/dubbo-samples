1a99e0bad9b91a92b04ac7973e95ffad5b1d69c7
kenstalks <77483349+kenstalks@users.noreply.github.com>
2021-01-21 16:22:14 +0800

    Improve api case  (#265)

--- dubbo-samples-api/src/main/java/org/apache/dubbo/samples/client/Application.java
+++ dubbo-samples-api/src/main/java/org/apache/dubbo/samples/client/Application.java
@@ -17,19 +17,22 @@
 
 package org.apache.dubbo.samples.client;
 
-
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.samples.api.GreetingsService;
 
 public class Application {
-    private static String zookeeperHost = System.getProperty("zookeeper.address", "127.0.0.1");
+    private static String zookeeperHost = System
+            .getProperty("zookeeper.address", "127.0.0.1");
+    private static String zookeeperPort = System.getProperty("zookeeper.port",
+            "2181");
 
     public static void main(String[] args) {
         ReferenceConfig<GreetingsService> reference = new ReferenceConfig<>();
         reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
-        reference.setRegistry(new RegistryConfig("zookeeper://" + zookeeperHost + ":2181"));
+        reference.setRegistry(new RegistryConfig(
+                "zookeeper://" + zookeeperHost + ":" + zookeeperPort));
         reference.setInterface(GreetingsService.class);
         GreetingsService service = reference.get();
         String message = service.sayHi("dubbo");
