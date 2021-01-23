78964c55495a6b65bfb7c7039236df478082f324
Chi Yuxing <yuxing.chi@ymh-group.com>
2021-01-23 19:59:53 +0800

    Fix test.

--- dubbo-samples-edas/dubbo-samples-edas-provider/src/main/java/org/apache/dubbo/samples/edas/provider/DubboProvider.java
+++ dubbo-samples-edas/dubbo-samples-edas-provider/src/main/java/org/apache/dubbo/samples/edas/provider/DubboProvider.java
@@ -23,11 +23,16 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import java.util.concurrent.CountDownLatch;
+
 @EnableAutoConfiguration
 @EnableDubbo(scanBasePackages = {"org.apache.dubbo.samples.edas"})
 public class DubboProvider {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(DubboProvider.class, args);
+
+        System.out.println("dubbo service started");
+        new CountDownLatch(1).await();
     }
 }
