5ca319d1d47f0b4b99e9da93573ce91fbc4fe6c8
xin gu <418294249@qq.com>
2022-11-08 16:58:35 +0800

    version
    version

--- dubbo-samples-boundary-test/dubbo-samples-port-unification-netty3/src/test/java/org/apache/dubbo/samples/DemoClientIT.java
+++ dubbo-samples-boundary-test/dubbo-samples-port-unification-netty3/src/test/java/org/apache/dubbo/samples/DemoClientIT.java
@@ -17,46 +17,21 @@ import java.util.HashMap;
 
 public class DemoClientIT {
 
-
-    static class Client implements Runnable {
-        private String protocol;
-        private String mode = "APPLICATION_FIRST";
-        Client(String protocol){
-            this.protocol = protocol;
-        }
-
-        Client(String protocol, String mode) {
-            this(protocol);
-            this.mode = mode;
-        }
-
-        @Override
-        public void run() {
-            ReferenceConfig<GreetingService> reference = new ReferenceConfig<>();
-            reference.setInterface(GreetingService.class);
-            reference.setParameters(new HashMap<>());
-            reference.getParameters().put("migration.step", mode);
-            reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
-            reference.setRegistry(new RegistryConfig(
-                    "zookeeper://" + "127.0.0.1" + ":" + "2181"));
-            reference.setProtocol(this.protocol);
-            GreetingService service = reference.get();
-            String message = service.sayHi(this.protocol);
-            System.out.println(message);
-            Assert.assertEquals("hi, tri",message);
-        }
-    }
-
     @Test
     public void testInterfaceDiscovery() throws InterruptedException {
-        DemoClientIT.Client dubboClient = new DemoClientIT.Client(CommonConstants.DUBBO);
-        DemoClientIT.Client triClient = new DemoClientIT.Client(CommonConstants.TRIPLE, "FORCE_INTERFACE");
-        Thread t1 = new Thread(dubboClient);
-        t1.start();
-        Thread t2 = new Thread(triClient);
-        t2.start();
-        t1.join();
-        t2.join();
+        ReferenceConfig<GreetingService> reference = new ReferenceConfig<>();
+        reference.setInterface(GreetingService.class);
+        reference.setParameters(new HashMap<>());
+        reference.getParameters().put("migration.step", "FORCE_INTERFACE");
+        reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
+        reference.setRegistry(new RegistryConfig(
+                "zookeeper://" + "127.0.0.1" + ":" + "2181"));
+        reference.setProtocol(CommonConstants.TRIPLE);
+        GreetingService service = reference.get();
+        String message = service.sayHi(CommonConstants.TRIPLE);
+        System.out.println(message);
+        Assert.assertEquals("hi, tri",message);
+
     }
 
 
