33a4e669e7bcb6b83984bb80ae49051a86979258
Albumen Kevin <jhq0812@gmail.com>
2022-11-08 20:27:29 +0800

    Enhance netty3 test

--- dubbo-samples-boundary-test/dubbo-samples-port-unification-netty3/src/test/java/org/apache/dubbo/samples/DemoClientIT.java
+++ dubbo-samples-boundary-test/dubbo-samples-port-unification-netty3/src/test/java/org/apache/dubbo/samples/DemoClientIT.java
@@ -1,19 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.samples;
-
 
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.samples.api.GreetingService;
-import org.apache.dubbo.samples.consumer.Application;
 
-import javassist.runtime.Inner;
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.HashMap;
 
 public class DemoClientIT {
 
@@ -21,12 +31,9 @@ public class DemoClientIT {
     public void testInterfaceDiscovery() throws InterruptedException {
         ReferenceConfig<GreetingService> reference = new ReferenceConfig<>();
         reference.setInterface(GreetingService.class);
-        reference.setParameters(new HashMap<>());
-        reference.getParameters().put("migration.step", "FORCE_INTERFACE");
         reference.setApplication(new ApplicationConfig("first-dubbo-consumer"));
         reference.setRegistry(new RegistryConfig(
                 "zookeeper://" + "127.0.0.1" + ":" + "2181"));
-        reference.setProtocol(CommonConstants.TRIPLE);
         GreetingService service = reference.get();
         String message = service.sayHi(CommonConstants.TRIPLE);
         System.out.println(message);
