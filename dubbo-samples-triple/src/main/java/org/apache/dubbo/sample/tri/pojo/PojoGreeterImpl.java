e930ae206290a28e7feaf373e6f235682d752b85
Albumen Kevin <jhq0812@gmail.com>
2022-11-08 17:10:55 +0800

    Fix license

--- dubbo-samples-triple/src/main/java/org/apache/dubbo/sample/tri/pojo/PojoGreeterImpl.java
+++ dubbo-samples-triple/src/main/java/org/apache/dubbo/sample/tri/pojo/PojoGreeterImpl.java
@@ -1,13 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dubbo.sample.tri.pojo;
 
-import org.apache.dubbo.common.Version;
 import org.apache.dubbo.common.stream.StreamObserver;
 import org.apache.dubbo.rpc.RpcContext;
-import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.sample.tri.Greeter;
 import org.apache.dubbo.sample.tri.api.PojoGreeter;
 import org.apache.dubbo.sample.tri.stub.GreeterImpl;
 import org.apache.dubbo.sample.tri.util.EchoStreamObserver;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
