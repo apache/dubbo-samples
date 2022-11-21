08f314c74a167c555e1513e0092201fad047e1a7
Albumen Kevin <jhq0812@gmail.com>
2022-11-21 11:48:13 +0800

    Fix license

--- dubbo-samples-boundary-test/dubbo-samples-test-10704/src/test/java/org/apache/dubbo/samples/local/DubboRequestFilter.java
+++ dubbo-samples-boundary-test/dubbo-samples-test-10704/src/test/java/org/apache/dubbo/samples/local/DubboRequestFilter.java
@@ -1,5 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.samples.local;
-
 
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
@@ -9,7 +24,6 @@ import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.Result;
 import org.apache.dubbo.rpc.RpcContext;
 import org.apache.dubbo.rpc.RpcException;
-import org.apache.dubbo.rpc.service.GenericService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,25 +35,8 @@ public class DubboRequestFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        String client = CommonConstants.PROVIDER;
-        if (RpcContext.getServiceContext().isConsumerSide()) {
-            client = CommonConstants.CONSUMER;
-        }
-        String baselog = "Client[" + client + "],InterfaceName=[" + invocation.getInvoker().getInterface().getSimpleName() + "],MethodName=[" + invocation.getMethodName() + "]";
-        log.info("DUBBO - 服务调用: {},Parameter={}", baselog, invocation.getArguments());
-
-        long startTime = System.currentTimeMillis();
-        // 执行接口调用逻辑
-        Result result = invoker.invoke(invocation);
-        // 调用耗时
-        long elapsed = System.currentTimeMillis() - startTime;
-        // 如果发生异常 则打印异常日志
-        if (result.hasException() && invoker.getInterface().equals(GenericService.class)) {
-            log.error("DUBBO - 服务异常: {},Exception={}", baselog, result.getException());
-        } else {
-            log.info("DUBBO - 服务响应: {},SpendTime=[{}ms]", baselog, elapsed);
-        }
-        return result;
+        RpcContext.getServiceContext().isConsumerSide();
+        return invoker.invoke(invocation);
     }
 
 }
