68773d618b724703f9ece4ec75d314d69148081f
changningbo <changningbo1995@gmail.com>
2022-11-29 16:25:52 +0800

    add apache licensed

--- dubbo-samples-spring-boot-3.x/dubbo-samples-spring-boot-3.x-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/ProviderApplication.java
+++ dubbo-samples-spring-boot-3.x/dubbo-samples-spring-boot-3.x-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/ProviderApplication.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.dubbo.springboot.demo.provider;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
