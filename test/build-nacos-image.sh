1e15be475524aaf6870ced1c52bd5db583c1687f
Albumen Kevin <jhq0812@gmail.com>
2023-01-04 16:20:55 +0800

    Fix image

---
+++ test/build-nacos-image.sh
@@ -0,0 +1,3 @@
+mkdir nacos-mysql
+curl -o nacos-mysql/Dockerfile https://raw.githubusercontent.com/nacos-group/nacos-docker/master/example/image/mysql/5.7/Dockerfile
+docker build -t nacos-mysql:5.7 nacos-mysql/
