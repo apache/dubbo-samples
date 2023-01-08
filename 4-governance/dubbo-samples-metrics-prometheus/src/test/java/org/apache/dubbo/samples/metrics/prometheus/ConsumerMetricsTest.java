7cd2c90c57f480156f817e9b59246551489290e7
songxiaosheng <songxiaosheng@elastic.link>
2023-01-08 16:47:09 +0800

    :white_check_mark: add test

--- 4-governance/dubbo-samples-metrics-prometheus/src/test/java/org/apache/dubbo/samples/metrics/prometheus/ConsumerMetricsTest.java
+++ 4-governance/dubbo-samples-metrics-prometheus/src/test/java/org/apache/dubbo/samples/metrics/prometheus/ConsumerMetricsTest.java
@@ -15,15 +15,41 @@
  * limitations under the License.
  */
 
-package org.apache.dubbo.samples.metrics;
+package org.apache.dubbo.samples.metrics.prometheus;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class MetricsServiceIT {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/dubbo-demo-provider.xml"})
+public class ConsumerMetricsTest {
+
+
+    private final String port = "20888";
 
     @Test
     public void test() throws Exception {
-
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet("http://localhost:" + port + "/metrics");
+            CloseableHttpResponse response = client.execute(request);
+            InputStream inputStream = response.getEntity().getContent();
+            String text = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+            Assert.assertTrue(text.contains("jvm_gc_memory_promoted_bytes_total"));
+        } catch (Exception e) {
+           Assert.fail(e.getMessage());
+        }
     }
 }
