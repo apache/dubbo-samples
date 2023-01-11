bbf0e646bd5a0ce727e31238363758c073e1e1e8
songxiaosheng <songxiaosheng@elastic.link>
2023-01-11 21:42:33 +0800

    :sparkles: Add Prometheus Metrics Sample

---
+++ 4-governance/dubbo-samples-metrics-prometheus/src/test/java/org/apache/dubbo/samples/metrics/prometheus/ProviderMetricsTest.java
@@ -0,0 +1,37 @@
+package org.apache.dubbo.samples.metrics.prometheus;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/dubbo-demo-provider.xml"})
+public class ProviderMetricsTest {
+
+    private  final String port = "20888";
+
+    @Test
+    public void test() throws Exception {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet("http://localhost:" + port + "/metrics");
+            CloseableHttpResponse response = client.execute(request);
+            InputStream inputStream = response.getEntity().getContent();
+            String text = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+            Assert.assertTrue(text.contains("jvm_gc_memory_promoted_bytes_total"));
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+}
