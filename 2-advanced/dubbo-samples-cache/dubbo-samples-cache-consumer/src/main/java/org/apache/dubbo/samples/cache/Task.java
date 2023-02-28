821fcb65f3f8da0eeae30569fc0bf6fd0a576438
Wu <dengwu.wu@foxmail.com>
2023-02-28 14:09:07 +0800

    fix: bug fix about task

--- 2-advanced/dubbo-samples-cache/dubbo-samples-cache-consumer/src/main/java/org/apache/dubbo/samples/cache/Task.java
+++ 2-advanced/dubbo-samples-cache/dubbo-samples-cache-consumer/src/main/java/org/apache/dubbo/samples/cache/Task.java
@@ -3,7 +3,9 @@ package org.apache.dubbo.samples.cache;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.samples.cache.api.CacheService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Task implements CommandLineRunner {
     @DubboReference(cache = "true")
     CacheService cacheService;
@@ -11,45 +13,45 @@ public class Task implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        new Thread(() -> {
-            String fix = null;
-            cacheService.findCache("0");
-            for (int i = 0; i < 5; i++) {
-                String result = cacheService.findCache("0");
-                if (fix == null || fix.equals(result)) {
-                    System.out.println("OK: " + result);
-                } else {
-                    System.err.println("ERROR: " + result);
-                }
-                fix = result;
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            // default cache.size is 1000 for LRU, should have cache expired if invoke more than 1001 times
-            for (int n = 0; n < 1001; n++) {
-                String pre = null;
-                cacheService.findCache(String.valueOf(n));
-                for (int i = 0; i < 10; i++) {
-                    String result = cacheService.findCache(String.valueOf(n));
-                    if (pre != null && !pre.equals(result)) {
-                        System.err.println("ERROR: " + result);
-                    }
-                    pre = result;
-                }
-            }
-
-            // verify if the first cache item is expired in LRU cache
+        System.out.println("ready to run task");
+        String fix = null;
+        cacheService.findCache("0");
+        for (int i = 0; i < 5; i++) {
             String result = cacheService.findCache("0");
-            if (fix != null && !fix.equals(result)) {
+            if (fix == null || fix.equals(result)) {
                 System.out.println("OK: " + result);
             } else {
                 System.err.println("ERROR: " + result);
             }
-        });
+            fix = result;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // default cache.size is 1000 for LRU, should have cache expired if invoke more than 1001 times
+        for (int n = 0; n < 1001; n++) {
+            String pre = null;
+            cacheService.findCache(String.valueOf(n));
+            for (int i = 0; i < 10; i++) {
+                String result = cacheService.findCache(String.valueOf(n));
+                if (pre != null && !pre.equals(result)) {
+                    System.err.println("ERROR: " + result);
+                }
+                pre = result;
+            }
+        }
+
+        // verify if the first cache item is expired in LRU cache
+        String result = cacheService.findCache("0");
+        if (fix != null && !fix.equals(result)) {
+            System.out.println("OK: " + result);
+        } else {
+            System.err.println("ERROR: " + result);
+        }
+
     }
 }
 
