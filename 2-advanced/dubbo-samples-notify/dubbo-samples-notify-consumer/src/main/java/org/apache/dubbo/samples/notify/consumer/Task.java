d8939ac644df8ef4f25a00af22e5084883187b8b
hjyp <2594297576@qq.com>
2023-03-01 15:35:09 +0800

    using annotation to config notify

--- 2-advanced/dubbo-samples-notify/dubbo-samples-notify-consumer/src/main/java/org/apache/dubbo/samples/notify/consumer/Task.java
+++ 2-advanced/dubbo-samples-notify/dubbo-samples-notify-consumer/src/main/java/org/apache/dubbo/samples/notify/consumer/Task.java
@@ -1,6 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package org.apache.dubbo.samples.notify.consumer;
 
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.annotation.Method;
 import org.apache.dubbo.samples.notify.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,7 +28,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class Task implements CommandLineRunner {
 
-    @DubboReference
+    // @DubboReference
+    @DubboReference(methods = @Method(name = "sayHello", onreturn = "notify.onReturn", onthrow = "notify.onThrow"))
     private DemoService demoService;
 
     @Autowired
