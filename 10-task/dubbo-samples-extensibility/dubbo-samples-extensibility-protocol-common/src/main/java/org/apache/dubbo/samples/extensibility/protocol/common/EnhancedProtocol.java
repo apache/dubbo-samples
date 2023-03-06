272dbb4817bdca85bfd0db8f6a8d77b79f02c95d
Xiong, Pin <pinxiong.cn@gmail.com>
2023-03-07 00:02:44 +0800

    feat: Support extensibility protocol task
    refer to issue #716

--- 10-task/dubbo-samples-extensibility/dubbo-samples-extensibility-protocol-common/src/main/java/org/apache/dubbo/samples/extensibility/protocol/common/EnhancedProtocol.java
+++ 10-task/dubbo-samples-extensibility/dubbo-samples-extensibility-protocol-common/src/main/java/org/apache/dubbo/samples/extensibility/protocol/common/EnhancedProtocol.java
@@ -1,16 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.samples.extensibility.protocol.common;
 
 import org.apache.dubbo.common.URL;
-import org.apache.dubbo.rpc.*;
+import org.apache.dubbo.rpc.Protocol;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Exporter;
+import org.apache.dubbo.rpc.ProtocolServer;
+import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.model.FrameworkModel;
-import org.apache.dubbo.rpc.protocol.tri.TripleProtocol;
+import org.apache.dubbo.rpc.protocol.dubbo.DubboProtocol;
 
 import java.util.List;
 
 public class EnhancedProtocol implements Protocol {
 
     public EnhancedProtocol(FrameworkModel frameworkModel) {
-        this.protocol = new TripleProtocol(frameworkModel);
+        this.protocol = new DubboProtocol(frameworkModel);
     }
 
     private final Protocol protocol;
@@ -23,14 +43,12 @@ public class EnhancedProtocol implements Protocol {
     @Override
     public <T> Exporter<T> export(Invoker<T> invoker) throws RpcException {
         // do something
-        //invoker.getUrl().setProtocol("tri");
         return this.protocol.export(invoker);
     }
 
     @Override
     public <T> Invoker<T> refer(Class<T> type, URL url) throws RpcException {
         // do something
-        //url.setProtocol("tri");
         return this.protocol.refer(type, url);
     }
 
--- 10-task/dubbo-samples-extensibility/dubbo-samples-extensibility-protocol-consumer/src/main/java/org/apache/dubbo/sample/extensibility/protocol/consume/ExtensibilityProtocolConsumerApplication.java
+++ 10-task/dubbo-samples-extensibility/dubbo-samples-extensibility-protocol-consumer/src/main/java/org/apache/dubbo/sample/extensibility/protocol/consume/ExtensibilityProtocolConsumerApplication.java
@@ -16,9 +16,10 @@
  */
 package org.apache.dubbo.sample.extensibility.protocol.consume;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableDubbo
 @SpringBootApplication
 public class ExtensibilityProtocolConsumerApplication {
     public static void main(String[] args) {
