f33516f71098dc505d591b2cfafc60b31d176ad4
hjyp <2594297576@qq.com>
2023-03-07 10:41:57 +0800

    modify case-configuration.yml

--- 2-advanced/dubbo-samples-group/dubbo-samples-group-provider/src/main/java/org/apache/dubbo/samples/group/provider/ProviderApplication.java
+++ 2-advanced/dubbo-samples-group/dubbo-samples-group-provider/src/main/java/org/apache/dubbo/samples/group/provider/ProviderApplication.java
@@ -25,6 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDubbo
 public class ProviderApplication {
     public static void main(String[] args) {
+        new EmbeddedZooKeeper(2181, false).start();
         SpringApplication.run(ProviderApplication.class, args);
     }
 }
