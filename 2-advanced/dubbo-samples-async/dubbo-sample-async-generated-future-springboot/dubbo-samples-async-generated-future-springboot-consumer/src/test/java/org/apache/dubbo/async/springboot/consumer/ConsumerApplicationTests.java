ea4907b03137a9ec2937ca7415167bb8b0999818
zy <953725892@qq.com>
2023-03-24 16:10:00 +0800

    feat(dubbo-sample-async-generated-future-springboot-consumer):init consumer module

--- 2-advanced/dubbo-samples-async/dubbo-sample-async-generated-future-springboot/dubbo-samples-async-generated-future-springboot-consumer/src/test/java/org/apache/dubbo/async/springboot/consumer/ConsumerApplicationTests.java
+++ 2-advanced/dubbo-samples-async/dubbo-sample-async-generated-future-springboot/dubbo-samples-async-generated-future-springboot-consumer/src/test/java/org/apache/dubbo/async/springboot/consumer/ConsumerApplicationTests.java
@@ -1,10 +1,10 @@
-package org.apache.dubbo.async.springboot.provider;
+package org.apache.dubbo.async.springboot.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProviderApplicationTests {
+class ConsumerApplicationTests {
 
 	@Test
 	void contextLoads() {
