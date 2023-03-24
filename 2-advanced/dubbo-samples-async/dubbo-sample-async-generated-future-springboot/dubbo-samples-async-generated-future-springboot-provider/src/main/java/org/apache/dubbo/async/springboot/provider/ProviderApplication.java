86e0591fa582df021990d4f7ce126e9850f50c96
zy <953725892@qq.com>
2023-03-24 11:55:15 +0800

    feat(dubbo-sample-async-generated-future-springboot-provider):init provider module

---
+++ 2-advanced/dubbo-samples-async/dubbo-sample-async-generated-future-springboot/dubbo-samples-async-generated-future-springboot-provider/src/main/java/org/apache/dubbo/async/springboot/provider/ProviderApplication.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo.async.springboot.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderApplication.class, args);
+	}
+
+}
---
+++ 2-advanced/dubbo-samples-async/dubbo-sample-async-generated-future-springboot/dubbo-samples-async-generated-future-springboot-provider/src/test/java/org/apache/dubbo/async/springboot/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo.async.springboot.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
