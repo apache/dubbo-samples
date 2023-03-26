a33120e6188f0a2d1b76ff640e1c67c42cf7d771
ax1an9 <cp3_hyx@foxmail.com>
2023-03-26 20:49:02 +0800

    modify provider

--- 2-advanced/dubbo-samples-async/dubbo-samples-async-simple-boot/dubbo-samples-async-simple-boot-provider/src/main/java/org/apache/dubbo/async/boot/provider/ProviderApplication.java
+++ 2-advanced/dubbo-samples-async/dubbo-samples-async-simple-boot/dubbo-samples-async-simple-boot-provider/src/main/java/org/apache/dubbo/async/boot/provider/ProviderApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDubbo
 public class ProviderApplication {
     public static void main(String[] args) {
-        new EmbeddedZooKeeper(2181, false).start();
+        //new EmbeddedZooKeeper(2181, false).start();
         SpringApplication.run(ProviderApplication.class,args);
     }
 }
