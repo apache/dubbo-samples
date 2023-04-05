39cffced4b63a34cde4d7574f4e314a3ace83dc0
Andy Cheung <micro_zx@msn.cn>
2023-04-05 23:42:12 +0800

    Remove invocation of deprecated org.junit.Assert.assertThat()

--- 2-advanced/dubbo-samples-mock/src/test/java/org/apache/dubbo/samples/mock/MockServicesIT.java
+++ 2-advanced/dubbo-samples-mock/src/test/java/org/apache/dubbo/samples/mock/MockServicesIT.java
@@ -26,7 +26,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:spring/mock-consumer.xml")
