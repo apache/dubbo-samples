f2bb59332a7ba3cd647086ab7f8c92b95e18d1f9
stellar <stellar.yu1999@gmail.com>
2023-10-13 18:40:57 +0800

    Provide different QoS addresses for consumers and producers to prevent errors when starting both on the same machine.(#1003)

--- 1-basic/dubbo-samples-api/src/main/java/org/apache/dubbo/samples/provider/Application.java
+++ 1-basic/dubbo-samples-api/src/main/java/org/apache/dubbo/samples/provider/Application.java
@@ -17,6 +17,7 @@
 
 package org.apache.dubbo.samples.provider;
 
+import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ProtocolConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.ServiceConfig;
@@ -29,12 +30,15 @@ public class Application {
     private static final String ZOOKEEPER_ADDRESS = "zookeeper://" + ZOOKEEPER_HOST + ":" + ZOOKEEPER_PORT;
 
     public static void main(String[] args) {
+        ApplicationConfig applicationConfig = new ApplicationConfig("first-dubbo-provider");
+        applicationConfig.setQosPort(33333);
+
         ServiceConfig<GreetingsService> service = new ServiceConfig<>();
         service.setInterface(GreetingsService.class);
         service.setRef(new GreetingsServiceImpl());
 
         DubboBootstrap.getInstance()
-                .application("first-dubbo-provider")
+                .application(applicationConfig)
                 .registry(new RegistryConfig(ZOOKEEPER_ADDRESS))
                 .protocol(new ProtocolConfig("dubbo", -1))
                 .service(service)
