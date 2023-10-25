d750f832c423d910152382240aa5f5907d5636df
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 13:41:10 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -3,6 +3,23 @@
 # Use mirror:
 # DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
+if [ -f k3s-install.sh ]; then
+  echo "k3s-install.sh exists"
+else
+  # 下载 k3s-install.sh 文件
+  wget -O k3s-install.sh https://get.k3s.io/
+
+  # 检查下载是否成功
+  if [ $? -eq 0 ]; then
+    echo "k3s-install.sh download success"
+    # 授予执行权限
+    chmod +x k3s-install.sh
+    ./k3s-install.sh --docker
+  else
+    echo "download k3s-install.sh fail"
+  fi
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/dubbo-test-runner/build.sh
