62b5246b9d97f66ff4ff143e6fd2c88396eeb114
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 14:18:14 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -3,19 +3,19 @@
 # Use mirror:
 # DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
-bash /usr/local/bin/k3s-uninstall.sh
-#if [ -e ./k3s-install.sh ]; then
-#    echo "k3s-install.sh exists"
-#else
-#    wget -O k3s-install.sh https://get.k3s.io/
-#    if [ $? -eq 0 ]; then
-#        echo "k3s-install.sh download success"
-#        chmod +x ./k3s-install.sh
-#        bash -x ./k3s-install.sh --docker
-#      else
-#        echo "download k3s-install.sh fail"
-#      fi
-#fi
+if [ -e ./k3s-install.sh ]; then
+    echo "k3s-install.sh exists"
+else
+    wget -O k3s-install.sh https://get.k3s.io/
+    if [ $? -eq 0 ]; then
+        echo "k3s-install.sh download success"
+        chmod +x ./k3s-install.sh
+        bash -x ./k3s-install.sh --docker
+        systemctl status k3s
+      else
+        echo "download k3s-install.sh fail"
+      fi
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
