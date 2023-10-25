208a78d7807ccdcc7290b4bcdfdc40c87451bb9d
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 15:25:27 +0800

    Setup K3d/K3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -3,26 +3,6 @@
 # Use mirror:
 # DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
-#bash /usr/local/bin/k3s/k3s-uninstall.sh
-#bash /usr/local/bin/k3s-uninstall.sh
-echo "="
-ls /usr/local/bin/
-echo "="
-#if [ -e ./k3s-install.sh ]; then
-#    echo "k3s-install.sh exists"
-#else
-#    wget -O k3s-install.sh https://get.k3s.io/
-#    if [ $? -eq 0 ]; then
-#        echo "k3s-install.sh download success"
-#        chmod +x ./k3s-install.sh
-#        bash -x ./k3s-install.sh --docker
-#        systemctl status k3s
-#        kubectl get nodes
-#
-#      else
-#        echo "download k3s-install.sh fail"
-#      fi
-#fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
