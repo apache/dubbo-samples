e1f3f59b6824ca695cc5f74d71d7fd8a1531a2c6
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 14:40:26 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -3,8 +3,9 @@
 # Use mirror:
 # DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
-bash /usr/local/bin/k3s/k3s-uninstall.sh
-bash /usr/local/bin/k3s-uninstall.sh
+#bash /usr/local/bin/k3s/k3s-uninstall.sh
+#bash /usr/local/bin/k3s-uninstall.sh
+ls /usr/local/bin/ | grep k3s
 kubectl get nodes
 if [ -e ./k3s-install.sh ]; then
     echo "k3s-install.sh exists"
