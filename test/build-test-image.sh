b209886dffe14b63dc22701f116748a2c9d91d87
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 14:46:16 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -8,21 +8,21 @@
 echo "="
 ls /usr/local/bin/
 echo "="
-if [ -e ./k3s-install.sh ]; then
-    echo "k3s-install.sh exists"
-else
-    wget -O k3s-install.sh https://get.k3s.io/
-    if [ $? -eq 0 ]; then
-        echo "k3s-install.sh download success"
-        chmod +x ./k3s-install.sh
-        bash -x ./k3s-install.sh --docker
-        systemctl status k3s
-        kubectl get nodes
-
-      else
-        echo "download k3s-install.sh fail"
-      fi
-fi
+#if [ -e ./k3s-install.sh ]; then
+#    echo "k3s-install.sh exists"
+#else
+#    wget -O k3s-install.sh https://get.k3s.io/
+#    if [ $? -eq 0 ]; then
+#        echo "k3s-install.sh download success"
+#        chmod +x ./k3s-install.sh
+#        bash -x ./k3s-install.sh --docker
+#        systemctl status k3s
+#        kubectl get nodes
+#
+#      else
+#        echo "download k3s-install.sh fail"
+#      fi
+#fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
