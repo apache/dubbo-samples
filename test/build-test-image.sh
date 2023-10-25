818e18c3120dfeb2296a68d207a8e1c2115ef9ef
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 13:55:52 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -6,15 +6,12 @@
 if [ -f k3s-install.sh ]; then
   echo "k3s-install.sh exists"
 else
-  # 下载 k3s-install.sh 文件
   wget -O k3s-install.sh https://get.k3s.io/
 
-  # 检查下载是否成功
   if [ $? -eq 0 ]; then
     echo "k3s-install.sh download success"
-    # 授予执行权限
     chmod +x k3s-install.sh
-    ./k3s-install.sh --docker
+    /bin/bash k3s-install.sh --docker
   else
     echo "download k3s-install.sh fail"
   fi
