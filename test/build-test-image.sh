4dae3993197b054e9d88856c29264504d632369a
MieAh <100568933+MieAh@users.noreply.github.com>
2023-10-25 14:06:07 +0800

    download k3s

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -3,16 +3,15 @@
 # Use mirror:
 # DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
 
-filename="k3s-install.sh"
 
-if [ -e "$filename" ]; then
+if [ -e ./k3s-install.sh ]; then
     echo "k3s-install.sh exists"
 else
-    wget -O "$filename" https://get.k3s.io/
+    wget -O k3s-install.sh https://get.k3s.io/
     if [ $? -eq 0 ]; then
         echo "k3s-install.sh download success"
-        chmod +x k3s-install.sh
-        /bin/bash -x k3s-install.sh --docker
+        chmod +x ./k3s-install.sh
+        bash -x ./k3s-install.sh --docker
       else
         echo "download k3s-install.sh fail"
       fi
