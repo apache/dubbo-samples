a38dfaf4bd6a6660c923627f955cd1129f89ed73
Albumen Kevin <jhq0812@gmail.com>
2023-11-30 17:50:58 +0800

    Add test cases to verify apache/dubbo#13442

---
+++ 99-integration/dubbo-samples-test-13442-1/dubbo-samples-test-13442-1-api/src/main/java/org/apache/dubbo/samples/test/User.java
@@ -0,0 +1,15 @@
+package org.apache.dubbo.samples.test;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
