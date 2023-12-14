f6620781d7a6773a45ea0814290eef84e7e89c96
chickenlj <ken.lj.hz@gmail.com>
2023-12-14 11:47:05 +0800

    update sample

--- 1-basic/dubbo-samples-spring-boot/dubbo-samples-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/DemoServiceImpl.java
+++ 1-basic/dubbo-samples-spring-boot/dubbo-samples-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/DemoServiceImpl.java
@@ -14,27 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dubbo.springboot.demo.provider.dubbo.service;
+package org.apache.dubbo.springboot.demo.provider;
 
-import org.apache.dubbo.springboot.demo.idl.DubboGreeterTriple.GreeterImplBase;
-import org.apache.dubbo.springboot.demo.idl.GreeterReply;
-import org.apache.dubbo.springboot.demo.idl.GreeterRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.springboot.demo.DemoService;
 
 @DubboService
-public class GreeterImpl extends GreeterImplBase {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GreeterImpl.class);
+public class DemoServiceImpl implements DemoService {
 
     @Override
-    public GreeterReply greet(GreeterRequest request) {
-        LOGGER.info("Server received greet request {}" , request);
-        return GreeterReply.newBuilder()
-                .setMessage("hello," + request.getName())
-                .build();
+    public String sayHello(String name) {
+        return "Hello " + name;
     }
-
 }
