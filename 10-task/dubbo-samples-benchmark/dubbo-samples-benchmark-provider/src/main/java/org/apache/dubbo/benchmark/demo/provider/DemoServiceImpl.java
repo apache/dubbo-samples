4a6d0b0b74953b6fd8afcadca128160e41268ce9
林尧 <wangxiaobin@cai-inc.com>
2024-02-06 19:46:34 +0800

    fix

--- 10-task/dubbo-samples-benchmark/dubbo-samples-benchmark-provider/src/main/java/org/apache/dubbo/benchmark/demo/provider/DemoServiceImpl.java
+++ 10-task/dubbo-samples-benchmark/dubbo-samples-benchmark-provider/src/main/java/org/apache/dubbo/benchmark/demo/provider/DemoServiceImpl.java
@@ -19,12 +19,17 @@ package org.apache.dubbo.benchmark.demo.provider;
 
 import org.apache.dubbo.benchmark.demo.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @DubboService
 public class DemoServiceImpl implements DemoService {
 
+    Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+
     @Override
     public String sayHello(String name) {
+        logger.info("invoke Hello " + name);
         return "Hello " + name;
     }
 }
