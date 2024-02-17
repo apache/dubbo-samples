a3ebab0ff849263ce1ce582c05a75a513b2f8ad1
foghost <foghost.cn@gmail.com>
2024-02-17 12:12:56 +0800

    add native test support

---
+++ test/dubbo-test-runner/src/native/run-dubbo-native-app.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DIR=/usr/local/dubbo/
+cd $DIR
+
+source $DIR/utils.sh
+
+# wait ports before run app: WAIT_PORTS_BEFORE_RUN=host:port;host:port
+if [ "$WAIT_PORTS_BEFORE_RUN" != "" ]; then
+  echo "Waiting ports before run native app .."
+  split_and_check_tcp_ports "$WAIT_PORTS_BEFORE_RUN" $SECONDS $WAIT_TIMEOUT
+  result=$?
+  if [ $result -ne 0 ]; then
+    echo "Wait ports before run native app failure"
+    exit $result
+  fi
+fi
+
+#delay start
+if [ $RUN_DELAY -gt 0 ]; then
+  echo "Delay $RUN_DELAY s."
+  sleep $RUN_DELAY
+fi
+
+cd $DIR/src/${SERVICE_DIR}
+echo "Build native app : ..."
+mvn package native:compile -Dmaven.test.skip=true -Pnative 2>&1
+result=$?
+if [ $result -ne 0 ]; then
+  echo "Build native app failure "
+  exit 1
+fi
+
+echo "Running native app : ..."
+start=$SECONDS
+./$DIR/src/${SERVICE_DIR}/target/${SERVICE_NAME} 2>&1 &
+pid=$!
+
+echo "Wait for process to exit: $pid .."
+wait $pid
+result=$?
+if [ $result -ne 0 ]; then
+  exit $result
+fi
+
