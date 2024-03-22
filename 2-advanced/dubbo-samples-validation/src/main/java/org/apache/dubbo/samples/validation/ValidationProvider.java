72b80a76c34bc75ddb9a150625777f15e0c0d586
MiSinG <1608839567@qq.com>
2024-03-22 18:32:01 +0800

    feat:start zookeeper

--- 2-advanced/dubbo-samples-validation/src/main/java/org/apache/dubbo/samples/validation/ValidationProvider.java
+++ 2-advanced/dubbo-samples-validation/src/main/java/org/apache/dubbo/samples/validation/ValidationProvider.java
@@ -27,6 +27,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValidationProvider {
 
     public static void main(String[] args) throws Exception {
+        new EmbeddedZooKeeper(2181, false).start();
         SpringApplication.run(ValidationProvider.class,args);
         System.out.println("dubbo service started");
     }
