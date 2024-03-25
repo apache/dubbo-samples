bc122356376b9e31b2d62631e51c9bfa4b4b5d72
wangxiaobin <wangxiaobin@cai-inc.com>
2024-03-25 11:13:59 +0800

    fix

--- test/build-nacos-image.sh
+++ test/build-nacos-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p nacos-mysql
-curl -o nacos-mysql/Dockerfile https://raw.githubusercontent.com/wxbty/nacos-docker/master/example/image/mysql/5.7-m1/Dockerfile
+curl -o nacos-mysql/Dockerfile https://raw.gitmirror.com/wxbty/nacos-docker/master/example/image/mysql/5.7-m1/Dockerfile
 docker build -t nacos-mysql:5.7 nacos-mysql/
