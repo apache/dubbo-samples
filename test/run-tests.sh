7747069f42ae2f89efeb71341055ea0816df118c
wxbty <wxb_101@163.com>
2024-04-03 21:28:40 +0800

    remove log

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -148,7 +148,6 @@ function run_test_with_version_profile() {
        jvm_opts="$version_profile $parameter_runtime -Dprop=\"$output_params\""
     fi
 
-    echo "parameter_runtime=$parameter_runtime"
     echo "jvm_opts=$jvm_opts"
     mvn $BUILD_OPTS $jvm_opts &> $project_home/mvn.log
     result=$?
