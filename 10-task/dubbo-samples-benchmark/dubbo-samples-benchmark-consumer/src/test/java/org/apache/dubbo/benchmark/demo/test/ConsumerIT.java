1f969b75fd8306ca517c04f144f474351bbd1eef
dyjjack <49814522+dyjjack@users.noreply.github.com>
2024-04-15 10:48:06 +0800

    skywalking只采样sample模式，不采样throughput模式 (#4)
    * skywalking只采样sample模式，不采样throughput模式
    
    * dyj test
    
    ---------
    
    Co-authored-by: duyujie <1053505543@qq.com>
    Co-authored-by: du <1045@qq.com>

--- 10-task/dubbo-samples-benchmark/dubbo-samples-benchmark-consumer/src/test/java/org/apache/dubbo/benchmark/demo/test/ConsumerIT.java
+++ 10-task/dubbo-samples-benchmark/dubbo-samples-benchmark-consumer/src/test/java/org/apache/dubbo/benchmark/demo/test/ConsumerIT.java
@@ -33,12 +33,11 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -50,13 +49,14 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class ConsumerIT {
 
     String propKey = "prop";
 
     @Test
-    public void test() throws RunnerException {
+    public void test() throws Exception {
 
         String prop = System.getProperty(propKey);
         String propJson = null;
@@ -78,21 +78,72 @@ public class ConsumerIT {
             prop = String.join("_", propList);
         }
 
-        Options options;
-        ChainedOptionsBuilder optBuilder = new OptionsBuilder()
-                .include(MyBenchmark.class.getSimpleName())
-                .param("time", System.currentTimeMillis() + "")
-                .param("prop", propJson == null ? "" : propJson)
-                .mode(Mode.Throughput)
-                .mode(Mode.SampleTime)
-                .threads(32)
-                .forks(1);
-
-        options = doOptions(optBuilder, prop).build();
-        new Runner(options).run();
+        String sampleFileName = runSample(propJson, prop);
 
         dotTrace(prop, propKey, propJson);
 
+        String throughputFileName = runThroughput(propJson);
+
+        mergeResult(sampleFileName, throughputFileName);
+
+    }
+
+    private static void mergeResult(String sampleFileName, String throughputFileName) throws IOException {
+        String sampleResultJson = FileUtils.readFileToString(new File(sampleFileName), "UTF-8");
+        String throughputResultJson = FileUtils.readFileToString(new File(throughputFileName), "UTF-8");
+
+        Gson gson = new Gson();
+        List firstResults = gson.fromJson(sampleResultJson, List.class);
+        List secondResults = gson.fromJson(throughputResultJson, List.class);
+
+        firstResults.addAll(secondResults);
+
+        String mergedResultJson = gson.toJson(firstResults);
+        FileUtils.writeStringToFile(new File(sampleFileName), mergedResultJson, "UTF-8");
+    }
+
+    private String runSample(String propJson, String prop) throws Exception {
+        String fileName;
+        if (StringUtils.isNotBlank(prop)) {
+            fileName = "/tmp/jmh_result_prop[" + prop + "]_" + UUID.randomUUID() + ".json";
+        } else {
+            fileName = "/tmp/jmh_result_" + UUID.randomUUID() + ".json";
+        }
+
+        Options options = new OptionsBuilder()
+                .include(MyBenchmark.class.getSimpleName())
+                .param("time", System.currentTimeMillis() + "")
+                .param("prop", propJson == null ? "" : propJson)
+                .resultFormat(ResultFormatType.JSON)
+                .result(fileName)
+                .mode(Mode.SampleTime)
+                .threads(32)
+                .forks(1)
+                .build();
+
+        new Runner(options).run();
+
+        return fileName;
+    }
+
+    private static String runThroughput(String propJson) throws Exception {
+        String fileName = "/tmp/" + UUID.randomUUID() + ".json";
+
+        Options options = new OptionsBuilder()
+                .include(MyBenchmark.class.getSimpleName())
+                .param("time", System.currentTimeMillis() + "")
+                .param("prop", propJson == null ? "" : propJson)
+                .resultFormat(ResultFormatType.JSON)
+                .result(fileName)
+                .jvmArgs("-Dzookeeper.address=zookeeper")
+                .mode(Mode.Throughput)
+                .threads(32)
+                .forks(1)
+                .build();
+
+        new Runner(options).run();
+
+        return fileName;
     }
 
     private static void dotTrace(String prop, String propKey, String propJson) {
@@ -159,7 +210,7 @@ public class ConsumerIT {
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("mysql test error");
+            System.out.println("dotTrace error");
         } finally {
             try {
                 if (resultSet != null) {
@@ -176,16 +227,6 @@ public class ConsumerIT {
             }
         }
 
-    }
-
-    private static ChainedOptionsBuilder doOptions(ChainedOptionsBuilder optBuilder, String prop) {
-        if (StringUtils.isNotBlank(prop)) {
-            optBuilder.result("/tmp/jmh_result_prop[" + prop + "].json");
-        } else {
-            optBuilder.result("/tmp/jmh_result.json");
-        }
-        optBuilder.resultFormat(ResultFormatType.JSON);
-        return optBuilder;
     }
 
     @State(Scope.Benchmark)
