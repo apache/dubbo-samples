8542b393816096423ff8637875baf595ebf5486f
Administrator <935076884@qq.com>
2024-04-23 13:42:56 +0800

    add apache license

--- dubbo-samples-shop/onlineShopping/vite.config.js
+++ dubbo-samples-shop/onlineShopping/vite.config.js
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // import { fileURLToPath, URL } from 'node:url'
 
 // import { defineConfig } from 'vite'
@@ -27,7 +43,7 @@
 //       scss: {
 //         // 自动导入定制化样式文件进行样式覆盖
 //         additionalData: `
-//           @use "@/styles/element/index.scss" as *;         
+//           @use "@/styles/element/index.scss" as *;
 //         `,
 //       }
 //     }
@@ -69,7 +85,7 @@ export default defineConfig({
         // 自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
-          @use "@/styles/element/var.scss" as *;       
+          @use "@/styles/element/var.scss" as *;
         `,
       }
     }
