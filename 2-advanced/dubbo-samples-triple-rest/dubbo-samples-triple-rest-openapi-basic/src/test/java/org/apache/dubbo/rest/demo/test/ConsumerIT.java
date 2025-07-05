fece708ad0e3e0424e7006c2bcdad4b2f367940c
zrlw <zrlw@sina.com>
2025-07-05 15:58:05 +0800

    FIx ConsumerIT of dubbo-samples-triple-rest-openapi-basic

--- 2-advanced/dubbo-samples-triple-rest/dubbo-samples-triple-rest-openapi-basic/src/test/java/org/apache/dubbo/rest/demo/test/ConsumerIT.java
+++ 2-advanced/dubbo-samples-triple-rest/dubbo-samples-triple-rest-openapi-basic/src/test/java/org/apache/dubbo/rest/demo/test/ConsumerIT.java
@@ -17,21 +17,21 @@
 
 package org.apache.dubbo.rest.demo.test;
 
-import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestClient;
 
 
-@SpringBootTest
+@EnableDubbo
+@ExtendWith(SpringExtension.class)
 public class ConsumerIT {
 
     private static final String HOST = System.getProperty("dubbo.address", "localhost");
     private final RestClient webClient = RestClient.create();
-
-    @DubboReference(url = "tri://${dubbo.address:localhost}:50052")
 
     private static String toUri(String path) {
         return "http://" + HOST + ":50052/" + path;
