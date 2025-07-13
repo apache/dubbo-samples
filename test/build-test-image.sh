fea0bf2eba3318a64cfb2101cc6213e4b436aa14
zrlw <zrlw@sina.com>
2025-07-13 11:13:06 +0800

    Build test image with mirrors.aliyun.com

--- test/build-test-image.sh
+++ test/build-test-image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Use mirror:
-# DEBIAN_MIRROR=http://mirrors.aliyun.com ./build-test-image.sh
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/dubbo-test-runner/build.sh
+# Use mirror:
+# DEBIAN_MIRROR=http://mirrors.aliyun.com $DIR/dubbo-test-runner/build.sh
+# No mirror:
+# $DIR/dubbo-test-runner/build.sh
+
+DEBIAN_MIRROR=http://mirrors.aliyun.com $DIR/dubbo-test-runner/build.sh
 
 $DIR/build-nacos-image.sh
