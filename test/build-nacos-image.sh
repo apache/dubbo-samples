d6672b895110eec425df34a71ff7d805b05ef8a9
zrlw <zrlw@sina.com>
2026-03-20 15:58:16 +0800

    Add nacos mysql schema sql file to docker file from github.com

--- test/build-nacos-image.sh
+++ test/build-nacos-image.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 mkdir nacos-mysql
-curl -o nacos-mysql/Dockerfile https://raw.githubusercontent.com/nacos-group/nacos-docker/master/example/image/mysql/5.7/Dockerfile
+
+#curl -o nacos-mysql/Dockerfile https://raw.githubusercontent.com/nacos-group/nacos-docker/master/example/image/mysql/5.7/Dockerfile
+
+#Since mysql-schema.sql is not found at https://raw.githubusercontent.com, we add it from github.com:
+cat > nacos-mysql/Dockerfile << EOF
+FROM mysql:5.7.40
+ADD https://github.com/alibaba/nacos/blob/master/distribution/conf/mysql-schema.sql /docker-entrypoint-initdb.d/nacos-mysql.sql
+RUN chown -R mysql:mysql /docker-entrypoint-initdb.d/nacos-mysql.sql
+EXPOSE 3306
+CMD ["mysqld", "--character-set-server=utf8mb4", "--collation-server=utf8mb4_unicode_ci"]
+EOF
+
 docker build -t nacos-mysql:5.7 nacos-mysql/
